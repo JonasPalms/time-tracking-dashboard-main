bc17b379c7d041ca389e015de13a21bbb11e8928
Jonas Sørensen <jonas.p.sorensen@gmail.com>
2022-01-03 15:32:18 +0100

    fixed undefined array

--- script.js
+++ script.js
@@ -3,67 +3,69 @@ const daily = document.getElementById("daily");
 const weekly = document.getElementById("weekly");
 const monthly = document.getElementById("monthly");
 
-
-// current time displays
-
-
 // previous time displays
-const previousWork = document.getElementById("work-previous");
-const previousPlay = document.getElementById("play-previous");
-const previousStudy = document.getElementById("study-previous");
-const previousExercise = document.getElementById("exercise-previous");
-const previousSocial = document.getElementById("social-previous");
-const previousSelfCare = document.getElementById("self-care-previous");
+const previousOutputs = document.querySelectorAll("#time__previous");
 
-// nu jeg tænker over det kunne jeg også bare have lavet et array af alle elementerne
-
+// current time displays 
 const currentOutputs = document.querySelectorAll("#time__current");
-console.log(currentOutputs);
 
-//event listeners
-daily.addEventListener("click", getData);
-weekly.addEventListener("click", getData);
-monthly.addEventListener("click", getData);
+//click listeners
+daily.addEventListener("click", updateUI);
+weekly.addEventListener("click", updateUI);
+monthly.addEventListener("click", updateUI);
 
-// pass the time selector to the function
-// select all relevant data points in the json file using the selector?
-// update the UI (new numbers and words)
-// use literal strings
-
-
+// declare globals variables
+let data = [];
 let previousTimes = [];
 let currentTimes = [];
 
-function getData(event) {
+// async func to fetch json data
+async function loadData() {
+    const response = await fetch('/data.json');
+    const fetchedData = await response.json();
+    data = fetchedData;
+}
 
-    const selector = event.target.id;
+function selectText(selector) {
 
+}
+
+
+function updateUI(event) {
+
+    // reset arrays if not empty 
     if (previousTimes || currentTimes) {
         previousTimes = [], currentTimes = [];
     }
 
-    fetch("./data.json")
-        .then(response => response.json())
-        .then(data => {
+    // selector for daily/weekly/monthly
+    const selector = event.target.id;
 
-            for (let i = 0; i < 6; i++) {
-                currentTimes.push(data[i].timeframes[selector].current);
-                previousTimes.push(data[i].timeframes[selector].previous);
-            }
+    // Select apporopriate text.. tried callBackFn but could not get it to work. Fix later!
+    let previousText = "";
+    if (selector === "daily") {
+        previousText = "Yesterday"
+    } else if (selector === "weekly") {
+        previousText = "Last week"
+    } else {
+        previousText = "Last month"
+    }
 
-        })
-        .catch(error => console.log(error));
+    // write new values to array
+    for (let i = 0; i < data.length; i++) {
+        currentTimes.push(data[i].timeframes[selector].current);
+        previousTimes.push(data[i].timeframes[selector].previous);
+    }
 
+    for (let i = 0; i < previousOutputs.length; i++) {
+        previousOutputs[i].innerText = `${previousText} - ${previousTimes[i]}hrs`;
+    }
 
-    updateIU(previousTimes, currentTimes);
+    for (let i = 0; i < currentOutputs.length; i++) {
+        currentOutputs[i].innerText = `${currentTimes[i]}hrs`;
+    }
 }
 
-function updateIU(previousTimes, currentTimes) {
 
-    console.log(previousTimes, currentTimes);
-
-    currentOutputs.forEach(output => {
-        output.innerText = `${currentTimes[0]}hrs`;
-    })
-}
-
+// load data on page load
+loadData();
